241dee2722690114213ba0d11d741609505ed4bc
Jons Lindau <jonas@jonika.nu>
2020-06-11 18:34:44 +0200

    Small refactoring

--- jBot.Lib/ServiceInstance.cs
+++ jBot.Lib/ServiceInstance.cs
@@ -6,17 +6,11 @@ namespace jBot.Lib
 {
     public class ServiceInstance
     {
-        private TwitterService Service;
-
         public ServiceInstance(AuthToken authToken)
         {
-            Service = new TwitterService(authToken.ConsumerKey, authToken.ConsumerKeySecret, authToken.AccessToken, authToken.AccessTokenSecret);
+            Instance = new TwitterService(authToken.ConsumerKey, authToken.ConsumerKeySecret, authToken.AccessToken, authToken.AccessTokenSecret);
         }
 
-        public TwitterService Instance => Service;
-
-        //public ServiceInstance(AuthToken authToken) => Instance = new TwitterService(authToken.ConsumerKey, authToken.ConsumerKeySecret, authToken.AccessToken, authToken.AccessTokenSecret);
-
-        //public TwitterService Instance { get; private set; }
+        public TwitterService Instance { get; }
     }
 }

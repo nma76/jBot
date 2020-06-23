a87a518b15996bbe222825f91b797f9c4db96c33
Jons Lindau <jonas@jonika.nu>
2020-06-23 14:03:03 +0200

    Added missing file to source control

---
+++ jBot.Lib/Business/SystemCommand/UptimeHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace jBot.Lib.Business.SystemCommand
+{
+    public static class UptimeHelper
+    {
+        public static string Uptime
+        {
+            get
+            {
+                string output = "";
+
+                try
+                {
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        return "Not implemented yet";
+                    }
+                    else
+                    {
+                        Process proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "uptime",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = true
+                            }
+                        };
+                        proc.Start();
+
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            output += proc.StandardOutput.ReadLine();
+                        }
+                        output = output.Substring(0, output.IndexOf(","));
+                        output = output.Substring(output.IndexOf(" "));
+                        return output;
+                    }
+
+                }
+                catch { }
+
+                return null;
+            }
+        }
+    }
+}

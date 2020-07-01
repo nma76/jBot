5da4e187b80ab02c8eaf2e12edee1d9f165d9e41
Jons Lindau <jonas@jonika.nu>
2020-07-01 15:05:41 +0200

    Removed unused code

--- jBot.Daemon/ConsoleHost.cs
+++ jBot.Daemon/ConsoleHost.cs
@@ -7,47 +7,6 @@ namespace jBot.Daemon
     public static class ConsoleHost
     {
         /// <summary>
-        /// Block the calling thread until shutdown is triggered via Ctrl+C or SIGTERM.
-        /// </summary>
-        //public static void WaitForShutdown()
-        //{
-        //    WaitForShutdownAsync().GetAwaiter().GetResult();
-        //}
-
-
-        /// <summary>
-        /// Runs an application and block the calling thread until host shutdown.
-        /// </summary>
-        /// <param name="host">The <see cref="IWebHost"/> to run.</param>
-        //public static void Wait()
-        //{
-        //    WaitAsync().GetAwaiter().GetResult();
-        //}
-
-        /// <summary>
-        /// Runs an application and returns a Task that only completes when the token is triggered or shutdown is triggered.
-        /// </summary>
-        /// <param name="host">The <see cref="IConsoleHost"/> to run.</param>
-        /// <param name="token">The token to trigger shutdown.</param>
-        //public static async Task WaitAsync(CancellationToken token = default(CancellationToken))
-        //{
-        //    //Wait for the token shutdown if it can be cancelled
-        //    if (token.CanBeCanceled)
-        //    {
-        //        await WaitAsync(token, shutdownMessage: null);
-        //        return;
-        //    }
-        //    //If token cannot be cancelled, attach Ctrl+C and SIGTERN shutdown
-        //    var done = new ManualResetEventSlim(false);
-        //    using (var cts = new CancellationTokenSource())
-        //    {
-        //        AttachCtrlcSigtermShutdown(cts, done, shutdownMessage: "Application is shutting down...");
-        //        await WaitAsync(cts.Token, "Application running. Press Ctrl+C to shut down.");
-        //        done.Set();
-        //    }
-        //}
-
-        /// <summary>
         /// Returns a Task that completes when shutdown is triggered via the given token, Ctrl+C or SIGTERM.
         /// </summary>
         /// <param name="token">The token to trigger shutdown.</param>
@@ -61,16 +20,6 @@ namespace jBot.Daemon
                 done.Set();
             }
         }
-
-        //private static async Task WaitAsync(CancellationToken token, string shutdownMessage)
-        //{
-        //    if (!string.IsNullOrEmpty(shutdownMessage))
-        //    {
-        //        Console.WriteLine(shutdownMessage);
-        //    }
-        //    await WaitForTokenShutdownAsync(token);
-        //}
-
 
         private static void AttachCtrlcSigtermShutdown(CancellationTokenSource cts, ManualResetEventSlim resetEvent, string shutdownMessage)
         {
@@ -88,7 +37,7 @@ namespace jBot.Daemon
                     }
                     catch (ObjectDisposedException) { }
                 }
-                //Wait on the given reset event
+                
                 resetEvent.Wait();
             };
 
@@ -96,7 +45,7 @@ namespace jBot.Daemon
             Console.CancelKeyPress += (sender, eventArgs) =>
             {
                 ShutDown();
-                //Don't terminate the process immediately, wait for the Main thread to exit gracefully.
+                
                 eventArgs.Cancel = true;
             };
         }

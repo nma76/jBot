0e05061c7d4bf95e87464fd626f39ddcb028c987
Jons Lindau <jonas@jonika.nu>
2020-07-01 15:04:34 +0200

    * ConsoleHost.cs: Helper class
    * Program.cs: Main class for daemon
    
    * jBot.Daemon.csproj: Linux daemon
    
    * jBot.sln: Aded daemon project

---
+++ jBot.Daemon/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace jBot.Daemon
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await ConsoleHost.WaitForShutdownAsync();
+        }
+    }
+}

e478788ad8d323158878738303bdf74e748dc1ce
Jons Lindau <jonas@jonika.nu>
2020-07-16 20:33:12 +0200

    Added logic for new capability

---
+++ jBot.Lib/Business/Extension/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace jBot.Lib.Business.Extension
+{
+    public static class Extensions
+    {
+        public static int ToIndexNumber(this string str, int max)
+        {
+            return str.ToCharArray().Sum(x => x) % max;
+        }
+    }
+}
---
+++ jBot.Lib/Business/NameGenerator/Generator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using jBot.Lib.Business.Extension;
+using jBot.Lib.Models;
+
+namespace jBot.Lib.Business.NameGenerator
+{
+    public class Generator
+    {
+        private readonly List<Person> _nameList;
+
+        public string OriginalName { get; set; }
+        public Person Referee
+        {
+            get
+            {
+                var namePos = OriginalName.ToIndexNumber(_nameList.Count - 1);
+                return _nameList[namePos];
+            }
+        }
+
+        public Generator(List<Person> nameList)
+        {
+            _nameList = nameList;
+        }
+    }
+}
